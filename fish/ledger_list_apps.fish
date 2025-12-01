955d631407d3604aa302850313edbecf4b6c8adc
Cordtus <Cordtus@gmail.com>
2025-11-30 19:08:37 -0700

    Consolidate helpers, add zsh, improve reliability
    - Consolidate separate scripts into single sourced files
    - Add zsh support
    - Remove lsusb dependency, use deviceInfo directly
    - Use node for reliable JS-to-JSON conversion
    - Remove emojis
    - Add semver comparison for firmware checks
    - Simplify connection wait logic
    - Update documentation

--- fish/ledger_list_apps.fish
+++
@@ -1,6 +0,0 @@
-function ledger_list_apps
-    # Return a list of installed app names (requires unlocked device)
-    ledger-live listApps \
-        | sed -E "s/'/\"/g; s/([[:alnum:]_]+):/\"\1\":/g" \
-        | jq -r '.[] .name'
-end
